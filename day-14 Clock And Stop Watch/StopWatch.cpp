beca2ea03eb6a8134d1b5fec6c3c7e90c6f78408
sarfaraz01576 <sarfaraz.8.kh@gmail.com>
2023-07-04 05:47:53 -0700

    100-Days-of-Code

--- day-14 Clock And Stop Watch/StopWatch.cpp
+++ day-14 Clock And Stop Watch/StopWatch.cpp
@@ -3,52 +3,61 @@
 #include <conio.h>
 using namespace std;
 
-int main(){
+int main()
+{
 	bool start = false;
 	int second = 0;
 	int minute = 0;
 	int hour = 0;
 	int range;
-	while(!start){
+	while (!start)
+	{
 		system("cls");
-		cout<< "\t\t\t\t\t" << hour <<" : " << minute <<" : " << second;
-	cout<<"\nif start  click on 'A'";
-	cout<<"\nif restart click on 'R'";
-	if(_kbhit()){
-		switch(_getch()){
+		cout << "\t\t\t\t\t" << hour << " : " << minute << " : " << second;
+		cout << "\nif start  click on 'A'";
+		cout << "\nif restart click on 'R'";
+		if (_kbhit())
+		{
+			switch (_getch())
+			{
 			case 'a':
 				start = true;
 			case 'r':
 				second = 0;
 				minute = 0;
 				hour = 0;
-		}
-	}
-	while(start){
-		Sleep(1000);
-		system("cls");
-		second += 1;
-		if(second == 60){
-			second = 0;
-			minute += 1;
-			if(minute == 60 ){
-				minute = 0;
-				hour += 1;
 			}
 		}
-		cout<< "\t\t\t\t\t" << hour <<" : " << minute <<" : " << second;
-		cout<<"\nif stop click on 'S'";
-		cout<<"\nif restart click on 'R'";
-		if(_kbhit()){
-		switch(_getch()){
-			case 's':
-				start = false;
-			case 'r':
+		while (start)
+		{
+			Sleep(1000);
+			system("cls");
+			second += 1;
+			if (second == 60)
+			{
 				second = 0;
-				minute = 0;
-				hour = 0;
+				minute += 1;
+				if (minute == 60)
+				{
+					minute = 0;
+					hour += 1;
+				}
+			}
+			cout << "\t\t\t\t\t" << hour << " : " << minute << " : " << second;
+			cout << "\nif stop click on 'S'";
+			cout << "\nif restart click on 'R'";
+			if (_kbhit())
+			{
+				switch (_getch())
+				{
+				case 's':
+					start = false;
+				case 'r':
+					second = 0;
+					minute = 0;
+					hour = 0;
+				}
+			}
 		}
 	}
-	}
-}
 }
